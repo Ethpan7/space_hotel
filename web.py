2d35ff7b4e84c93902bad4cd4185174311dd03b9
Alyssa J. Lee <alyjlee@alyssas-macpoop.local>
2023-03-15 10:24:09 +0800

    Database is now functional for sign up page, sign-in is also working.

--- web.py
+++ web.py
@@ -5,6 +5,12 @@ import sqlite3
 app = Flask(__name__)
 app.debug = True
 
+#connecting to sqlite
+conn = sqlite3.connect('data.db')
+
+#establishing cursor
+c = conn.cursor()
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -28,7 +34,37 @@ def signup():
     return render_template('signup.html')
 
 
-@app.route('/data-entry', methods=['GET', 'POST'])
+
+@app.route('/signin', methods=['GET', 'POST'], endpoint='signin')
+def signin():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(password)
+
+        conn = sqlite3.connect('data.db')
+        c = conn.cursor()
+
+        # Retrieve user with the matching username
+        select_query = "SELECT * FROM users WHERE username = ?"
+        c.execute(select_query, (username,))
+        user = c.fetchone()
+        print(user[5])
+
+        if user is not None:
+            # if password matches, allow sign-in
+            if user[5] == password:
+                return 'Sign-in successful'
+            else:
+                return 'Incorrect password'
+        else:
+            return 'User does not exist'
+
+    return render_template('signin.html')
+
+
+
+@app.route('/data-entry', methods=['GET', 'POST'], endpoint='data_entry')
 def data_entry():
     if request.method == 'POST':
         creditcard_info = request.form['credit_card_info']
@@ -41,4 +77,4 @@ def data_entry():
     return render_template('data_entry.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5001)
